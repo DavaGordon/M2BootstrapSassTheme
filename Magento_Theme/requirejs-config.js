53274f4f5cefd4970d4df8606a7f011b64247a62
suttonsilver-dgordon <d.gordon@suttonsilver.co.uk>
2017-01-10 14:41:46 +0000

    Initial commit

---
+++ Magento_Theme/requirejs-config.js
@@ -0,0 +1,10 @@
+var config = {
+    paths: {
+        "jquery.bootstrap": "web/bower_components/bootstrap-sass/assets/javascripts/bootstrap"
+    },
+    shim: {
+        'jquery.bootstrap': {
+            'deps': ['jquery']
+        }
+    }
+};
